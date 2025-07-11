f534e47cf47a3b89375d79deb7d56bab7637c49f
Ashwin Kashyap <cybernytrix@gmail.com>
2025-07-10 23:32:46 -0700

    Deploy new build artifacts - 2025-07-11T06:32:46Z

--- bookmarklet-loader.js
+++ bookmarklet-loader.js
@@ -1,7 +1,7 @@
 (function() {
     'use strict';
     const script = document.createElement('script');
-    script.src = 'https://cdn.jsdelivr.net/gh/AshKash/kaynix-artifacts@main/kaynix-core.4f544d37d4c7d7645633.js';
+    script.src = 'https://cdn.jsdelivr.net/gh/AshKash/kaynix-artifacts@main/kaynix-core.2d387209779c78c06633.js';
     script.async = true;
     document.head.appendChild(script);
 })();
